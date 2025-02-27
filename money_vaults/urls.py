925e9a744dc67ccd0f5d5cb498e17dfcc50d1cd7
SalahMuhammad <salahnazeer93@gmail.com>
2025-02-27 11:21:04 +0200

    change name to payment_method

--- money_vaults/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MoneyVaultListCreateView, MoneyVaultDetailView
-
-
-urlpatterns = [
-    path('', MoneyVaultListCreateView.as_view(), name='money_vault_list'),
-    path('<int:pk>/', MoneyVaultDetailView.as_view(), name='money_vault_detail'),
-]
