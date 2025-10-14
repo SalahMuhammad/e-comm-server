0b0e1b8bb15d202d04794915b867fc8108198f1a
SalahMuhammad <salahnazeer93@gmail.com>
2025-10-14 15:04:23 +0300

    unwanted file

--- finance/payments/ursl.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ListCreateView
-
-
-urlpatterns = [
-    path('', ListCreateView.as_view(), name='ursl-list-create'),
-    # path('ursl/<int:pk>/', ListCreateView.as_view(), name='ursl-detail'),
-]
