163fa51d44afe2e9a7d0c13fa819b575fcc16fdf
SalahMuhammad <salahnazeer93@gmail.com>
2025-10-21 16:35:58 +0300

    change expeneses endpoint url to reverse-payment

--- finance/payments/urls.py
+++ finance/payments/urls.py
@@ -4,7 +4,8 @@ from .views import ListCreateView, DetailPaymentView, ListCreateExpensePaymentVi
 
 urlpatterns = [
     path('payments/', ListCreateView.as_view()),
+    # path('payments/p/total/', ListCreateView.as_view()),
     path('payments/<str:pk>/', DetailPaymentView.as_view()),
-    path('expenses/', ListCreateExpensePaymentView.as_view()),
-    path('expenses/<int:pk>/', DetailExpensePaymentView.as_view()),
+    path('reverse-payment/list/', ListCreateExpensePaymentView.as_view()),
+    path('reverse-payment/<str:pk>/', DetailExpensePaymentView.as_view()),
 ]
