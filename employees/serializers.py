3f4d6afe83444ef73050f060b0c2092dd6550690
SalahMuhammad <salahnazeer93@gmail.com>
2026-06-24 11:36:12 +0300

    (update) add object representation

--- employees/serializers.py
+++ employees/serializers.py
@@ -3,8 +3,12 @@ from .models import Employee
 
 
 class EmployeeSerializers(serializers.ModelSerializer):
+    obj_representation = serializers.SerializerMethodField()
 
 
     class Meta:
         model = Employee
         fields = '__all__'
+
+    def get_obj_representation(self, obj):
+        return obj.first_name + ' ' + obj.last_name
